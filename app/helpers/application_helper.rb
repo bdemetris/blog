51a58ac505d166a1490c7d0e7163d9d74e2596a1
Brett <brett.demetris@airbnb.com>
2015-11-02 11:06:34 -0800

    Added Rouge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,33 @@
 module ApplicationHelper
+
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+
+  def block_code(code, language)
+    Rouge.highlight(code, language || 'text', 'html')
+  end
+end
   
   def markdown(text)
-    options = {
+    render_options = {
       filter_html:     true,
       hard_wrap:       true, 
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true, 
-      fenced_code_blocks: true
+      link_attributes: { rel: 'nofollow' }
     }
+    renderer = HTML.new(render_options)
 
     extensions = {
       autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
+      fenced_code_blocks: true,
+      lax_spacing:        true,
+      no_intra_emphasis:  true,
+      strikethrough:      true,
+      superscript:        true
     }
-
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
-
-    markdown.render(text).html_safe
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
 end
