480ce3e1b0060c1f04c9a03329d5a05b3eb9cad0
Brett <brett.demetris@airbnb.com>
2015-12-19 15:17:19 -0800

    fixing pre-complied objects in assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( profile.jpg )
-Rails.application.config.assets.precompile += %w( logo.svg )
+Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.precompile += %w( *.svg )
